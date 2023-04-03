05f8143d7ad65d97a3defe5465f6a3d819b6fd66
Aeeeeeep <aep_sun@qq.com>
2023-04-03 14:03:10 +0800

    init

---
+++ download_img.py
@@ -0,0 +1,47 @@
+import json
+import urllib.request
+from urllib.request import ProxyHandler, build_opener
+import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+
+proxy_handler = ProxyHandler({'socks5': '127.0.0.1:1089'})
+opener = build_opener(proxy_handler)
+
+def download_image(image_url, reviewer_id):
+    try:
+        with opener.open(image_url) as url:
+            image_data = url.read()
+        if not os.path.exists('images'):
+            os.makedirs('images')
+        with open(f'images/{reviewer_id}.jpg', 'wb') as img_file:
+            img_file.write(image_data)
+    except Exception as e:
+        print(f'Error downloading image {image_url}: {e}')
+        with open('404.csv', 'a') as f:
+            f.write(f'{reviewer_id},"{image_url}"\n')
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as f:
+        for line in f:
+            yield json.loads(line)
+
+max_workers = 64
+executor = ThreadPoolExecutor(max_workers=max_workers)
+
+tasks = []
+
+id_list = []
+images_list = os.listdir('./Automotive_5_images')
+for image_list in images_list:
+    id_list.append(image_list.split('.')[0])
+
+for review in tqdm(read_json_file('./Automotive_5.json')):
+    if ('image' in review) and (review['reviewerID'] not in id_list):
+        images = review['image']
+        for i, image_url in enumerate(images):
+            image_url = image_url.replace("_SY88.", "")
+            tasks.append(executor.submit(download_image, image_url, review['reviewerID']))
+
+for future in tqdm(as_completed(tasks), total=len(tasks)):
+    pass
---
+++ get_label.py
@@ -0,0 +1,60 @@
+import re
+import csv
+import json
+import numpy as np
+from tqdm import tqdm
+from textblob import TextBlob
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
+lock = threading.Lock()
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as f:
+        for line in f:
+            yield json.loads(line)
+
+def get_label(review, f):
+    reviewerID = review['reviewerID']
+    if 'vote' in review and int(review['vote'].replace(',','')) >= 5 and 'reviewText' in review and 'summary' in review:
+        vote = int(review['vote'].replace(',',''))
+        text = review['reviewText']
+        summary = review['summary']
+        blob_text = TextBlob(text)
+        blob_summary = TextBlob(summary)
+        text_list = []
+        summary_list = []
+        for sentence in blob_text.sentences:
+            text_list.append(sentence.sentiment.polarity)
+        for sentence in blob_summary.sentences:
+            summary_list.append(sentence.sentiment.polarity)
+        s = 0.3 * np.array(text_list).mean() + 0.7 * np.array(summary_list).mean()
+        text_summary = re.sub('[^\da-zA-Z\s\.,!?]', '', text+summary).replace('\n','')
+        if len(text.split())>=3 and len(summary.split())>=1:
+            if s<0 and review['overall']<=3.0:
+                data = [reviewerID, str(text_summary), vote, 0.0]
+            elif s>0 and review['overall']>=4.0:
+                data = [reviewerID, str(text_summary), vote, 1.0]
+            else:
+                return
+            lock.acquire()
+            try:
+                writer = csv.writer(f)
+                writer.writerow(data)
+            finally:
+                lock.release()
+
+with open('label.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(["ID","text","vote","label"])
+
+# for review in tqdm(read_json_file('./Books_5.json')):
+#     get_label(review)
+
+max_workers = 16
+executor = ThreadPoolExecutor(max_workers=max_workers)
+tasks = []
+with open('label.csv', 'a') as f:
+    for review in tqdm(read_json_file('./Books_5.json')):
+        tasks.append(executor.submit(get_label, review, f))
+    for futures in tqdm(as_completed(tasks), total=len(tasks)):
+        pass
