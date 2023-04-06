cf6fc939cc1e64f7a8170083ba6a5dff8de31459
aeeeeeep <aeeeeeep@proton.me>
2023-04-06 08:50:53 +0800

    add Multimodel baseline

--- tools/get_label.py
+++ tools/get_label.py
@@ -1,5 +1,6 @@
 import re
 import csv
+import os
 import time
 import json
 import numpy as np
@@ -14,16 +15,16 @@ def read_json_file(file_path):
         for line in f:
             yield json.loads(line)
 
-def get_label(review, f):
+def get_label(review, f, img_list):
     reviewerID = review['reviewerID']
-    if 'vote' in review and int(review['vote'].replace(',','')) >= 5 and 'reviewText' in review and 'summary' in review:
+    if 'reviewText' in review and 'summary' in review:
         vote = int(review['vote'].replace(',',''))
         text = review['reviewText']
         summary = review['summary']
-        asin = review['asin']
-        reviewtime = int(review['unixReviewTime'])
-        reviewtime = time.localtime(reviewtime)
-        reviewtime = time.strftime("%Y-%m-%d %H", reviewtime)
+        # asin = review['asin']
+        # reviewtime = int(review['unixReviewTime'])
+        # reviewtime = time.localtime(reviewtime)
+        # reviewtime = time.strftime("%Y-%m-%d %H", reviewtime)
         blob_text = TextBlob(text)
         blob_summary = TextBlob(summary)
         text_list = []
@@ -32,38 +33,47 @@ def get_label(review, f):
             text_list.append(sentence.sentiment.polarity)
         for sentence in blob_summary.sentences:
             summary_list.append(sentence.sentiment.polarity)
-        s = 0.3 * np.array(text_list).mean() + 0.7 * np.array(summary_list).mean()
+        s = 0.7 * np.array(text_list).mean() + 0.3 * np.array(summary_list).mean()
         # s = np.array(summary_list).mean()
-        text_summary = re.sub('[^\da-zA-Z\s\.,!?]', '', summary).replace('\n','')
-        if len(text.split())>=3 and len(summary.split())>=1:
-            if s<0 and review['overall']<=3.0:
-                data = [reviewerID, str(text_summary), asin, reviewtime, vote, 0.0]
+        text_summary = re.sub('[^\da-zA-Z\s\.,!?]', '', summary + text).replace('\n','')
+        if len(text.split())>=5 and len(summary.split())>=1:
+        # if (reviewerID in img_list) and len(text.split())>=10 and len(summary.split())>=1:
+            if s<=-0.01 and review['overall']<=3.0:
+                data = [reviewerID, str(text_summary), 0.0]
                 # data = [reviewerID, asin, reviewtime, 0.0]
-            elif s>0 and review['overall']>=4.0:
-                data = [reviewerID, str(text_summary), asin, reviewtime, vote, 1.0]
+            elif s>=0.01 and review['overall']>=4.0:
+                data = [reviewerID, str(text_summary), 2.0]
                 # data = [reviewerID, asin, reviewtime, 1.0]
-            else:
-                return
-            lock.acquire()
-            try:
-                writer = csv.writer(f)
-                writer.writerow(data)
-            finally:
-                lock.release()
+            elif s<0.01 and s>-0.01 and review['overall']==4.0:
+                data = [reviewerID, str(text_summary), 1.0]
+        else:
+            return
+        lock.acquire()
+        try:
+            writer = csv.writer(f)
+            writer.writerow(data)
+        finally:
+            lock.release()
 
-with open('label.csv', 'a') as f:
+with open('label_0.csv', 'a') as f:
     writer = csv.writer(f)
-    writer.writerow(["ID","summary","asin","time","vote","label"])
+    writer.writerow(["ID","text","label"])
     # writer.writerow(["ID","asin","time","label"])
 
 # for review in tqdm(read_json_file('./Books_5.json')):
 #     get_label(review)
 
-max_workers = 16
+max_workers = 120
 executor = ThreadPoolExecutor(max_workers=max_workers)
 tasks = []
-with open('label.csv', 'a') as f:
-    for review in tqdm(read_json_file('../data/Books_5.json')):
-        tasks.append(executor.submit(get_label, review, f))
+
+img_list = []
+for i in os.listdir("../data/Books_5_images"):
+    img_list.append(i[:-4])
+
+with open('label_0.csv', 'a') as f:
+    for review in tqdm(read_json_file('../data/S_0.json')):
+    # for review in tqdm(read_json_file('../data/Books_5_img.json')):
+        tasks.append(executor.submit(get_label, review, f, img_list))
     for futures in tqdm(as_completed(tasks), total=len(tasks)):
         pass
